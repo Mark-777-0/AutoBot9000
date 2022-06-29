43238ec378f617c276bb00a8011d10ee60623b71
Mark D <83738282+markusmark1@users.noreply.github.com>
2022-06-29 11:12:59 -0700

    Migrate

--- AutoBot9000/image_scraper/scraper.py
+++ AutoBot9000/image_scraper/scraper.py
@@ -4,7 +4,7 @@ try:
     import praw
 except ImportError:
     print ("Unable to find praw. Install here https://github.com praw-dev/praw")
-    raise
+    raise('Dependency Error: No Praw')
 
 from time import sleep
 from urllib import urlopen
@@ -17,12 +17,12 @@ import string
 _REDDIT_API_SLEEP_TIME = 2.50
 _VALID_CHARS = frozenset(''.join(("-_.() ", string.ascii_letters, string.digits)))
 
-def sanitize(s, default_name="image"):
-    sanitized = ''.join(c for c in s if c in _VALID_CHARS)
-    return sanitized[:0xFF] if sanitized else default_name # Use default if string is empty.
+def remove_chars(s, default_name="image"):
+    remove_charsd = ''.join(c for c in s if c in _VALID_CHARS)
+    return remove_charsd[:0xFF] if remove_charsd else default_name # Use default if string is empty.
 
-def unique(filename):
-    """Return a guaranteed-unique version of a given filename."""
+def unique_file(filename):
+
     if not os.path.exists(filename):
         return filename
     else:
@@ -64,16 +64,16 @@ def fetch_image(submission, directory):
     votes = '+%s,-%s' % (submission.ups, submission.downs)
     url = submission.url
     extension = url.split('.')[-1]
-    title = sanitize(submission.title) # Remove illegal characters
-    if title.endswith('.'): title = title[:-1] # Fix foo..jpg
-    local_filename = unique(os.path.join(directory, '%s.%s' % (title, extension)))
+    title = remove_chars(submission.title) # Remove illegal characters
+    if title.endswith('.'): title = title[:-1] 
+    local_filename = unique_file(os.path.join(directory, '%s.%s' % (title, extension)))
     directory_data = scan_hash(directory)
     download_and_save(url, local_filename, directory_data)
 
-def get_submissions(subred, limit, timeframe):
+def get_submissions(subreddit, limit, timeframe):
     methname = 'get_top_from_%s' % timeframe
-    if hasattr(subred, methname):
-        return getattr(subred, methname)(limit=limit)
+    if hasattr(subreddit, methname):
+        return getattr(subreddit, methname)(limit=limit)
     else:
         raise ValueError('Unrecognized timeframe: %s' %  timeframe)
 
@@ -94,8 +94,8 @@ def scrape(settings, include_sub=None, include_dir=None, timeframe='day', limits
 
             extensions = set(subreddit.file_types)
             limit = subreddit.num_files if limits is None else limits
-            subred = r.get_subreddit(subreddit.name)
-            submissions = get_submissions(subred, limit, timeframe)
+            subreddit = r.get_subreddit(subreddit.name)
+            submissions = get_submissions(subreddit, limit, timeframe)
 
             count = 0
             for sub in submissions:
@@ -104,8 +104,9 @@ def scrape(settings, include_sub=None, include_dir=None, timeframe='day', limits
                     fetch_image(sub, dirname)
                     count += 1
             yield count
-
-            sleep(_REDDIT_API_SLEEP_TIME) # Avoid offending the Reddit API 
+            
+            #sleep
+            sleep(_REDDIT_API_SLEEP_TIME)
 
 if __name__ == '__main__':
     settings = settings_.Settings()
--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 <h1>Reddit AutoBot9000 </h1>
 
+![reddit](https://github.com/Mark-777-0/AutoBot9000/blob/main/AutoBot9000/img/GUI.png)
+
+<div center>
+
 ![reddit](https://github.com/Mark-777-0/AutoBot9000/blob/main/AutoBot9000/img/example.gif)
 
+</div>
 There are two parts to this bot, the auto_poster and the image_scraper. 
 
 <h2> Selenium Poster</h2>
@@ -28,6 +33,13 @@ pip install praw
 ```
  Pip install tcl
 ```
+```
+ Pip install tk
+```
+Mac/Linux
+```
+sudo apt-get install python3-tk
+```
 =======
 
 <h2> Fill in Settings</h2>
@@ -43,6 +55,6 @@ The AutoBot9000 will allow you to download the top images from any subreddit. Sp
 
 Uses Python Reddit API Wrapper: https://github.com/praw-dev/praw
 
-Credit to chloe-47/reddit_scraper for help with the scraper section of this bot
+Credit to chloe-47/reddit_scraper for structual help with the scraper section of this bot
 
 Uses Selenium: https://www.selenium.dev/documentation/
